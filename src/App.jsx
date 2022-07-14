521474a6860d37d53ed2c61f84bae492a38e1d42
oiwa1108 <gamma.1108@gmail.com>
2022-07-14 22:54:46 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { Card } from "./components/Card";
+
+export const App = () => {
+    //管理者フラグ
+    const [isAdmin, setIsAdmin] = useState(false);
+
+    //[切り替え]押下時
+    const onClickSwitch = () => setIsAdmin(!isAdmin);
+
+    return (
+        <div>
+            {/* 管理者フラグがtrueの時とそれ以外で文字を出し分け */}
+            {isAdmin ? <span>管理者です</span> : <span>管理者以外です</span>}
+            <button onClick={onClickSwitch}>切り替え</button>
+            <Card isAdmin={isAdmin} />
+        </div>
+    );
+};
